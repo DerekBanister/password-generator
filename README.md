a726ee2c3c5105191f939fa087f336d25d21bbd2
Derek Banister <ptownderek@yahoo.com>
2021-09-23 18:39:37 -0700

    Changed confirm prompts to a while loop with if statements, updated README

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Password Generator
 
-This project generates a random password user based on the criteria they have chosen. It includes upper and lowercase letters, special symbols, and numbers.
+============================================================================================================================================================
+
+### This project generates a random password user based on the criteria they have chosen including upper and lowercase letters, special symbols, and numbers. It then takes all of the user's input and generates that password and displays it into the text area provided.
+
+
+
+![Password Generator](password-gen.png)
 
 ## Built With
 
@@ -10,13 +16,13 @@ This project generates a random password user based on the criteria they have ch
 
 ## Deployed Link
 
-* [See Live Site](#)
+* [Generate your very own secure password here!](https://derekbanister.github.io/password-generator/)
 
 
 ## Authors
 
-* **Derek Banister** 
+#### Derek Banister
 
-- [Link to Portfolio Site](#)
-- [Link to Github](https://github.com/)
-- [Link to LinkedIn](https://www.linkedin.com/)
+- [Link to Portfolio Site](https://derekbanister.github.io/First-Portfolio/)
+- [Link to Github](https://github.com/DerekBanister)
+- [Link to LinkedIn](https://www.linkedin.com/in/derek-banister/)
--- script.js
+++ script.js
@@ -8,10 +8,10 @@ var numbers = '1234567890';
 //setting password variable to empty so I can fill it based on user input
 var password = "";
 
-var lowersSelection;
-var uppersSelection;
-var symbolsSelection;
-var numbersSelection;
+var lowersSelection = false;
+var uppersSelection = false;
+var symbolsSelection = false;
+var numbersSelection = false;
 var promptLength;
 var charSelection;
 //So i can call button to prompt 
@@ -26,19 +26,32 @@ generateBtn.addEventListener("click", function() { // anonymous function (no dec
 function prompts() {
   promptLength = prompt("How many characters would you like your password to be");
   while (!promptLength || promptLength < 8 || promptLength > 128) {
-    if (confirmLength === null) {
-        break;
-  }
+ 
 }
-  lowersSelection = confirm("Would you like lowercase characters in your password");
-  uppersSelection = confirm ("Would you like uppercase characters in your password");
-  symbolsSelection = confirm ("Would you like special characters in your password");
-  numbersSelection = confirm ("Would you like numbers in your password");
+  
+  if (promptLength) {
+    if (confirm("Would you like lowercase characters in your password") == true){
+      lowersSelection = true;
+    }
+      if (confirm("Would you like uppercase characters in your password") == true){
+      uppersSelection = true;
+      }
+      if (confirm("Would you like special characters in your password") == true){
+      symbolsSelection = true;
+      }
+      if (confirm("Would you like numbers in your password") == true){
+      numbersSelection = true;
+      }
+      if (lowersSelection === false && uppersSelection === false && symbolsSelection === false && numbersSelection === false){
+        alert("You must select atleast one character type")
+      }
+    }
+ 
   
   console.log(promptLength, lowersSelection, uppersSelection, symbolsSelection, numbersSelection);
 
 }
-
+//taking user input from function prompts and adding them to the empty string of charselection
 function passGenerate() {
   charSelection = "";
   if (lowersSelection) {
@@ -55,7 +68,7 @@ function passGenerate() {
   }
   console.log(charSelection);
 }
-  
+  //takes both my global variables (charSelection and promptLength) and used math.floor and math random to generate password based on user inputs
 function passwordCreate(){
   password = "";
   for (var i = 0; i < promptLength; i++) {
@@ -66,7 +79,7 @@ function passwordCreate(){
 return password;
 
 }
-
+// code given in assignment
 function writePassword() {
 var password = passwordCreate();
  var passwordText = document.querySelector("#password");
